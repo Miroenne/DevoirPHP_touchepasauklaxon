43c891863e33c5fe1b2204163b81ef1977dfd5f6
Math <mmenin8306@icloud.com>
2026-09-02 11:00:51 +0200

    Finished functions in userController.php and tested it

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-use App\Exceptions\{ForbiddenException, UnthorizedException, InvalidArgumentException, RessourceNotFoundException};
+use App\Exceptions\{ForbiddenException, UnthorizedException, InvalidArgumentException, InvalidCredentialsException, RessourceNotFoundException};
 use App\Services\UserService;
 use Override;
 
@@ -47,6 +47,99 @@ class UserController extends Controller
             $error = $this->serialize->serializeException($e->getMessage(), $e->getStatusCode());
             return json_encode($error);
         } catch (InvalidArgumentException $e) {
+            $error = $this->serialize->serializeException($e->getMessage(), $e->getStatusCode());
+            return json_encode($error);
+        } catch (ForbiddenException $e) {
+            $error = $this->serialize->serializeException($e->getMessage(), $e->getStatusCode());
+            return json_encode($error);
+        } catch (RessourceNotFoundException $e) {
+            $error = $this->serialize->serializeException($e->getMessage(), $e->getStatusCode());
+            return json_encode($error);
+        }
+    }
+
+    public function loginController(): string
+    {
+
+        $service = new UserService;
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        try {
+            $connect = $service->login($email, $password);
+
+            setcookie(
+                'csrf-token',
+                $connect['csrfToken'],
+                [
+                    'expires' => time() + 60 * 60 * 24,
+                    'path' => '/',
+                    'httponly' => true,
+                    'secure' => true,
+                    'samesite' => 'none'
+                ]
+            );
+
+            $user = $connect['user'];
+            $result = ['user' => $user, 'responseCode' => 200];
+
+            return json_encode($result);
+        } catch (InvalidCredentialsException $e) {
+            $error = $this->serialize->serializeException($e->getMessage(), $e->getStatusCode());
+            return json_encode($error);
+        }
+    }
+
+    public function logoutController(): string
+    {
+        $service = new UserService();
+        $id = $_POST['id'];
+        $userId = $_POST['userId'];
+
+        if ($id !== $userId) {
+            $error = [
+                'message' => "Une erreur s'est produite",
+                'responseCode' => '400'
+            ];
+
+            return json_encode($error);
+        }
+
+        try {
+            $user = $service->findByIdService($id, $userId);
+
+            if (ini_get('session.use_cookies')) {
+                $p = session_get_cookie_params();
+                setcookie(
+                    session_name(),
+                    '',
+                    [
+                        'expires' => time() - 42000,
+                        'path' => $p['path'],
+                        'domain' => $p['domain'],
+                        'secure' => $p['secure'],
+                        'httponly' => $p['httponly'],
+                        'samesite' => $p['samesite']
+                    ]
+                );
+            }
+
+            setcookie(
+                'csrf-token',
+                '',
+                [
+                    'expires' => time() - 3600,
+                    'path' => '/',
+                    'httponly' => true,
+                    'secure' => true,
+                    'samesite' => 'none'
+                ]
+            );
+
+            session_destroy();
+            $res = ['message' => 'Déconnexion réussie', 'responseCode' => 200];
+            return json_encode($res);
+        } catch (UnthorizedException $e) {
             $error = $this->serialize->serializeException($e->getMessage(), $e->getStatusCode());
             return json_encode($error);
         } catch (ForbiddenException $e) {
